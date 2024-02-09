aee0035941000a74fa9b20556683051d8e9039f5
SEOJIWOO_TOKTOKHAN_DEV <a20304645@gmail.com>
2024-02-09 18:03:51 +0900

    feat: openGraph setting

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,6 +2,32 @@ import { ProductContainer } from '@/containers/product';
 import { Product } from '@/types/product';
 import { Suspense } from 'react';
 import Loading from './loading';
+import { Metadata, ResolvingMetadata } from 'next';
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const id = params.id;
+  const product = (await fetch(`https://fakestoreapi.com/products/${id}`).then(
+    (res) => res.json(),
+  )) as Product;
+  const productImage = product.image;
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: product.title,
+    openGraph: {
+      title: product.title,
+      images: [productImage, ...previousImages],
+    },
+  };
+}
 
 export async function generateStaticParams() {
   const products = (await fetch('https://fakestoreapi.com/products').then(
